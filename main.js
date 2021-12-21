b2339de510658343286ae28181fc03d205bd4c5c
yolocat-dev <developer@yolocat.dev>
2021-12-21 17:32:43 +0100

    handlers

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+const DiscordJS = require('discord.js')
+const client = new DiscordJS.Client({intents: ["GUILDS", "GUILD_MESSAGES"]})
+require('dotenv').config()
 
+client.commands = new DiscordJS.Collection()
+client.events = new DiscordJS.Collection()
+
+const handlers = ['command', 'event'];
+
+handlers.forEach(handler => {
+    require(`./handlers/${handler}`)(client, DiscordJS)
+})
+
+client.login(process.env.BOT_TOKEN)
